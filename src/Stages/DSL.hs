882b7782696d1e374b73d7498eedcc6795011f30
Rickard Lindberg <ricli85@gmail.com>
2014-12-17 20:41:27 +0100

    Analyze stage is now done in DSL stage.

--- src/Stages/DSL.hs
+++ src/Stages/DSL.hs
@@ -1,5 +1,6 @@
 module Stages.DSL
-    ( Stream
+    ( compileProgram
+    , Stream
     , Expression
     , Output
     , (=:)
@@ -8,23 +9,28 @@ module Stages.DSL
     , isEven
     , not
     , stringConstant
-    , compileProgram
     ) where
 
 import Control.Monad.State
 
 import Prelude hiding (not)
 import qualified Types.AST as AST
-import Stages.Analyze (astToStreams)
+import qualified Types.DAG as DAG
 import Stages.CodeGen (streamsToC)
 import Types.Phantom
 
-(=:) :: Output a -> Stream a -> State AST.Program ()
+compileProgram :: Action () -> IO ()
+compileProgram state = do
+    let x = execState state (DAGState 1 DAG.emptyStreams)
+    let cCode = streamsToC (dag x)
+    writeFile "main.c" cCode
+
+(=:) :: Output a -> Stream a -> Action ()
 (=:) output stream = do
-    modify $ addStatement $ AST.Assignment (unOutput output) (unStream stream)
-    where
-        addStatement :: AST.Statement -> AST.Program -> AST.Program
-        addStatement x (AST.Program xs) = AST.Program $ xs ++ [x]
+    restName <- buildNewStream (unStream stream)
+    thisName <- buildStream "pin" (DAG.OutputPin (unOutput output))
+    buildDependency restName thisName
+    buildInput thisName restName
 
 clock :: Stream Int
 clock = Stream $ AST.Builtin "clock"
@@ -43,9 +49,63 @@ isEven = Expression . AST.Even . unExpression
 stringConstant :: String -> Expression String
 stringConstant = Expression . AST.StringConstant
 
-compileProgram :: State AST.Program () -> IO ()
-compileProgram state = do
-    let ast = execState state (AST.Program [])
-    let streams = astToStreams ast
-    let cCode = streamsToC streams
-    writeFile "main.c" cCode
+data DAGState = DAGState
+    { idCounter :: Int
+    , dag       :: DAG.Streams
+    }
+
+type Action a = State DAGState a
+
+buildNewStream :: AST.Stream -> Action DAG.Identifier
+buildNewStream stream = case stream of
+    (AST.Builtin name) -> do
+        buildBuiltinStream name
+    (AST.Custom [input] expression) -> do
+        lastName <- buildNewStream input
+        thisName <- buildStream "stream" (DAG.Transform expression)
+        buildDependency lastName thisName
+        buildInput thisName lastName
+        return thisName
+
+buildInput :: DAG.Identifier -> DAG.Identifier -> Action ()
+buildInput srcStream inputStream = do
+    modify $ nodesAddInput srcStream inputStream
+    where
+        nodesAddInput :: String -> String -> DAGState -> DAGState
+        nodesAddInput nodeName inputName nodes = nodes
+            { dag = DAG.addInput (dag nodes) nodeName inputName
+            }
+
+buildDependency :: DAG.Identifier -> DAG.Identifier -> Action ()
+buildDependency srcStream destStream = do
+    modify $ nodesAddDependency srcStream destStream
+    where
+        nodesAddDependency :: String -> String -> DAGState -> DAGState
+        nodesAddDependency nodeName dependencyName nodes = nodes
+            { dag = DAG.addOutput (dag nodes) nodeName dependencyName
+            }
+
+buildBuiltinStream :: DAG.Identifier -> Action DAG.Identifier
+buildBuiltinStream name = do
+    streamTreeState <- get
+    unless (DAG.hasStream (dag streamTreeState) name) $ do
+        modify $ insertStream $ DAG.Stream name [] (DAG.Builtin name) []
+    return name
+
+buildStream :: String -> DAG.Body -> Action DAG.Identifier
+buildStream baseName body = do
+    uniqName <- buildUniqIdentifier baseName
+    modify $ insertStream $ DAG.Stream uniqName [] body []
+    return uniqName
+
+insertStream :: DAG.Stream -> DAGState -> DAGState
+insertStream stream x = x { dag = DAG.addStream (dag x) stream }
+
+buildUniqIdentifier :: String -> Action DAG.Identifier
+buildUniqIdentifier baseName = do
+    dag <- get
+    let id = idCounter dag
+    modify inc
+    return $ baseName ++ "_" ++ show id
+    where
+        inc dag = dag { idCounter = idCounter dag + 1 }
